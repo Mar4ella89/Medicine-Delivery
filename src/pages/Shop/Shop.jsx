ac97d1f49a7d55203ebd785028c82c52558e673c
Marat Baikin <3371335@gmail.com>
2024-04-15 17:38:12 +0300

    created medicines list

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,46 +1,79 @@
+import { useEffect, useState } from 'react';
+
 import Container from 'components/Container/Container';
+import { allMedicines } from 'services/medicinesAPI';
 
 import css from './Shop.module.css';
 
 const Shop = () => {
+  const [medicineItems, setMedicineItems] = useState([]);
+
+  useEffect(() => {
+    const fetchAllMedicines = async () => {
+      try {
+        const data = await allMedicines();
+        setMedicineItems(data);
+      } catch ({ response }) {
+        console.log(response.data.message);
+      }
+    };
+    fetchAllMedicines();
+  }, []);
+
+  console.log(medicineItems);
+
+  const elements = medicineItems.map(({ id, name, imgUrl, price }) => (
+    <li className={css.itemCart}>
+      <img src={imgUrl} alt={name} width={290} className={css.img} />
+      <h3 className={css.subtitle}>{name}</h3>
+      <div className={css.medicineOrder}>
+        <p>{price} $</p>
+        <button className={css.btnOrder}>add to Cart</button>
+      </div>
+    </li>
+  ));
+
   return (
     <Container>
-    <div className={css.wrapper}>
-      <section className={css.sectionDrug}>
-        <h1 className={css.title}>drugstore:</h1>
-        <ul>
-        <li className={css.item}>Drug 1</li>
-        <li className={css.item}>Drug 2</li>
-        <li className={css.item}>Drug 3</li>
-        <li className={css.item}>Drug 4</li>
-        <li className={css.item}>Drug 5</li>
-        </ul>
-        
-      </section>
-      <section className={css.sectionMedicine}>
-      <ul className={css.listCart}>
-        <li className={css.itemCart}>
-            <img src="../public/logo512.png" alt="" width={290} className={css.img}/>
-            <h3 className={css.subtitle}>Medicine 1</h3>
-            <div className={css.medicineOrder}>
-            <p>100$</p>
-            <button className={css.btnOrder}>add to Cart</button>
-            </div>
-            
+      <div className={css.wrapper}>
+        <section className={css.sectionDrug}>
+          <h1 className={css.title}>drugstore:</h1>
+          <ul>
+            <li className={css.item}>Drug 1</li>
+            <li className={css.item}>Drug 2</li>
+            <li className={css.item}>Drug 3</li>
+            <li className={css.item}>Drug 4</li>
+            <li className={css.item}>Drug 5</li>
+          </ul>
+        </section>
+        <section className={css.sectionMedicine}>
+          <ul className={css.listCart}>
+            {elements}
+            {/* <li className={css.itemCart}>
+              <img
+                src={medicineItems.imgUrl}
+                alt=""
+                width={290}
+                className={css.img}
+              />
+              <h3 className={css.subtitle}>medicineItems.name</h3>
+              <div className={css.medicineOrder}>
+                <p>medicineItems.price</p>
+                <button className={css.btnOrder}>add to Cart</button>
+              </div>
             </li>
-        <li className={css.itemCart}>Medicine 2</li>
-        <li className={css.itemCart}>Medicine 3</li>
-        <li className={css.itemCart}>Medicine 4</li>
-        <li className={css.itemCart}>Medicine 5</li>
-        <li className={css.itemCart}>Medicine 6</li>
-        <li className={css.itemCart}>Medicine 7</li>
-        <li className={css.itemCart}>Medicine 8</li>
-        <li className={css.itemCart}>Medicine 9</li>
-        <li className={css.itemCart}>Medicine 10</li>
-        </ul>
-        
-      </section>
-    </div>
+            <li className={css.itemCart}>Medicine 2</li>
+            <li className={css.itemCart}>Medicine 3</li>
+            <li className={css.itemCart}>Medicine 4</li>
+            <li className={css.itemCart}>Medicine 5</li>
+            <li className={css.itemCart}>Medicine 6</li>
+            <li className={css.itemCart}>Medicine 7</li>
+            <li className={css.itemCart}>Medicine 8</li>
+            <li className={css.itemCart}>Medicine 9</li>
+            <li className={css.itemCart}>Medicine 10</li> */}
+          </ul>
+        </section>
+      </div>
     </Container>
   );
 };
---
+++ src/services/medicinesAPI.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+const instance = axios.create({
+  baseURL: 'https://medicine-delivery-ztb4.onrender.com/api/medicines',
+  //   params: {
+  //     api_key: 'b331236c37988e13f653df9937f18de4',
+  //     language: 'en-US',
+  //     include_adult: false,
+  //   },
+});
+
+export const allMedicines = async () => {
+  const { data } = await instance.get(`/`);
+  return data;
+};
+
+// export const trendingMovie = async () => {
+//   const mediaType = 'all';
+//   const timeWindow = 'day';
+//   const { data } = await instance.get(`/trending/${mediaType}/${timeWindow}`);
+
+//   return data;
+// };
+
+// export const getMovieById = async id => {
+//   const { data } = await instance.get(`/movie/${id}`);
+//   return data;
+// };
+
+// export const searchMovie = async (searchQuery, page) => {
+//   const { data } = await instance.get(`/search/movie`, {
+//     params: {
+//       query: searchQuery,
+//       page,
+//     },
+//   });
+//   return data;
+// };
+
+// export const getMovieCast = async id => {
+//   const { data } = await instance.get(`/movie/${id}/credits`);
+//   return data;
+// };
+
+// export const getMovieReview = async (id, page) => {
+//   const { data } = await instance.get(`/movie/${id}/reviews`, {
+//     params: {
+//       page,
+//     },
+//   });
+//   return data;
+// };
+
+// getMovieById.propTypes = {
+//   id: PropTypes.string.isRequired,
+// };
+
+// searchMovie.propTypes = {
+//   searchQuery: PropTypes.string.isRequired,
+//   page: PropTypes.string.isRequired,
+// };
+
+// getMovieCast.propTypes = {
+//   id: PropTypes.string.isRequired,
+// };
+
+// getMovieReview.propTypes = {
+//   id: PropTypes.string.isRequired,
+//   page: PropTypes.string.isRequired,
+// };
