dcbc27537b2805da994add6be7ebd8a5e9d96ee1
Marat Baikin <3371335@gmail.com>
2024-04-16 17:11:26 +0300

    replace MedicineCardList

---
+++ src/components/MedicineCardList/MedicineCardList.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+import { allMedicines } from 'services/medicinesAPI';
+
+import css from './MedicineCardList.module.css';
+
+const MedicineCardList = () => {
+  const [medicineItems, setMedicineItems] = useState([]);
+
+  useEffect(() => {
+    const fetchAllMedicines = async () => {
+      try {
+        const data = await allMedicines();
+        setMedicineItems(data);
+      } catch ({ response }) {
+        console.log(response.data.message);
+      }
+    };
+    fetchAllMedicines();
+  }, []);
+
+  const elements = medicineItems.map(({ _id, name, imgUrl, price }) => (
+    <li key={_id} className={css.itemMedicine}>
+      <img src={imgUrl} alt={name} width={290} className={css.img} />
+      <h3 className={css.subtitle}>{name}</h3>
+      <div className={css.medicineOrder}>
+        <p>{price} $</p>
+        <button className={css.btnOrder}>add to Cart</button>
+      </div>
+    </li>
+  ));
+
+  return (
+    <section className={css.sectionMedicine}>
+      <ul className={css.listMedicine}>{elements}</ul>
+    </section>
+  );
+};
+
+export default MedicineCardList;
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 import Container from 'components/Container/Container';
-import { allMedicines } from 'services/medicinesAPI';
+import MedicineCardList from 'components/MedicineCardList/MedicineCardList';
 
 import css from './Shop.module.css';
 
 const Shop = () => {
-  const [medicineItems, setMedicineItems] = useState([]);
-
-  useEffect(() => {
-    const fetchAllMedicines = async () => {
-      try {
-        const data = await allMedicines();
-        setMedicineItems(data);
-      } catch ({ response }) {
-        console.log(response.data.message);
-      }
-    };
-    fetchAllMedicines();
-  }, []);
-
-  const elements = medicineItems.map(({ _id, name, imgUrl, price }) => (
-    <li key={_id} className={css.itemMedicine}>
-      <img src={imgUrl} alt={name} width={290} className={css.img} />
-      <h3 className={css.subtitle}>{name}</h3>
-      <div className={css.medicineOrder}>
-        <p>{price} $</p>
-        <button className={css.btnOrder}>add to Cart</button>
-      </div>
-    </li>
-  ));
-
   return (
     <Container>
       <div className={css.wrapper}>
@@ -44,9 +19,7 @@ const Shop = () => {
             <li className={css.item}>Drug 5</li>
           </ul>
         </section>
-        <section className={css.sectionMedicine}>
-          <ul className={css.listMedicine}>{elements}</ul>
-        </section>
+        <MedicineCardList />
       </div>
     </Container>
   );
