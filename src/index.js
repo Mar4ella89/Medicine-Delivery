fbd46fd10bd1c0a71ae1b07a508103e2c108b538
Marat Baikin <3371335@gmail.com>
2024-04-24 16:32:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/medicine-delivery"> */}
-    <App />
-    {/* </BrowserRouter> */}
+    <BrowserRouter basename="/medicine-delivery">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
