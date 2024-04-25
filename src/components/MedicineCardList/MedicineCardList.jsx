16c613abc1e54ec05fea759070ae831bc520fe00
Marat Baikin <3371335@gmail.com>
2024-04-25 17:50:00 +0300

    Update MedicineCardList.jsx

--- src/components/MedicineCardList/MedicineCardList.jsx
+++ src/components/MedicineCardList/MedicineCardList.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+
 import css from './MedicineCardList.module.css';
 
 const MedicineCardList = ({ medicines }) => {
+  const [pickedMedicine, setPickedMedicine] = useState([]);
+
+  const handleMedicineClick = idMedicines => {
+    const itemMedecine = medicines.filter(({ _id }) => _id === idMedicines);
+    setPickedMedicine(prevState => [...prevState, ...itemMedecine]);
+  };
+
   const elements = medicines.map(({ _id, name, imgUrl, price }) => (
     <li key={_id} className={css.itemMedicine}>
       <img src={imgUrl} alt={name} width={290} className={css.img} />
       <h3 className={css.subtitle}>{name}</h3>
       <div className={css.medicineOrder}>
         <p>{price} $</p>
-        <button className={css.btnOrder}>add to Cart</button>
+        <button
+          className={css.btnOrder}
+          onClick={() => handleMedicineClick(_id)}
+        >
+          add to Cart
+        </button>
       </div>
     </li>
   ));
