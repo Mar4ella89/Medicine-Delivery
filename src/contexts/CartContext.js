1c2290556c9e68a279e750aba8f6df1be9b72b02
Marat Baikin <3371335@gmail.com>
2024-04-29 18:00:56 +0300

    Update CartContext.js

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,19 +4,24 @@ const CartContext = createContext({
   cartItems: [],
   addToCart: () => {},
   removeFromCart: () => {},
+  //   quantity: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (medicines, idMedicines) => {
-    const itemMedecine = medicines.filter(({ _id }) => _id === idMedicines);
+  const addToCart = (medicines, medicineId) => {
+    const itemMedecine = medicines.filter(({ _id }) => _id === medicineId);
     setCartItems(prevState => [...prevState, ...itemMedecine]);
   };
 
   const removeFromCart = medicineId => {
     setCartItems(cartItems.filter(({ _id }) => medicineId !== _id));
   };
+
+  //   const updateQuantity = medicineId => {
+  //     cartItems.filter(_id => medicineId === _id);
+  //   };
 
   return (
     <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
