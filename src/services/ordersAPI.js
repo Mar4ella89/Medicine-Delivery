f2bfd966045bd254f1b07416ec9728476202754a
Marat Baikin <3371335@gmail.com>
2024-07-15 17:48:41 +0300

    Create ordersAPI.js

---
+++ src/services/ordersAPI.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://medicine-delivery-ztb4.onrender.com/api/orders',
+  params: {
+    limit: 12,
+  },
+});
+
+export const allOrders = async () => {
+  const { data } = await instance.get(`/`);
+  return data;
+};
+
+export const addOrders = async data => {
+  const { data: result } = await instance.post('/orders', data);
+  return result;
+};
