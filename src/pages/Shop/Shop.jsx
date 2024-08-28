a322a4b6b779ec0221961e88819fe45bf9c207fb
Marat Baikin <3371335@gmail.com>
2024-08-28 17:40:11 +0300

    Update Shop.jsx

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
 
 import Container from 'components/Container/Container';
 import MedicineCardList from 'components/MedicineCardList/MedicineCardList';
@@ -9,7 +8,6 @@ import DrugList from 'components/DrugList/DrugList';
 import DrugContext from 'contexts/DrugContext';
 import CartContext from 'contexts/CartContext';
 
-// import { allMedicines } from 'services/medicinesAPI';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './Shop.module.css';
 
@@ -17,35 +15,12 @@ const Shop = () => {
   const { selectedPharmacy, setSelectedPharmacy, drugsItem } =
     useContext(DrugContext);
 
-  const { medicineItems, filter, getVisibleMedicines, visibleMedicines } =
-    useContext(CartContext);
-
-  // const [medicineItems, setMedicineItems] = useState([]);
-  // getVisibleMedicines();
-  // useEffect(() => {
-  //   getVisibleMedicines();
-  // }, []);
-
-  // const getVisibleMedicines = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return medicineItems.filter(({ name }) => {
-  //     return name.toLowerCase().includes(normalizedFilter);
-  //   });
-  // };
-
-  // const visibleMedicines = getVisibleMedicines();
-  console.log(medicineItems);
-  console.log(visibleMedicines);
+  const { visibleMedicines } = useContext(CartContext);
 
   const filteredMedicines = visibleMedicines.filter(medicine => {
     const pharmacyIdArr = medicine.availablePharmacies;
     return pharmacyIdArr.includes(selectedPharmacy);
   });
-
-  // const [filter, setFilter] = useState('');
-
-  // const changeFilter = event => setFilter(event.currentTarget.value);
 
   return (
     <Container>
@@ -54,9 +29,6 @@ const Shop = () => {
           drugsItem={drugsItem}
           onSelectPharmacy={pharmacyId => setSelectedPharmacy(pharmacyId)}
         />
-        {/* <MedicineCardList
-          medicines={selectedPharmacy ? filteredMedicines : medicineItems}
-        /> */}
         <MedicineCardList
           medicines={selectedPharmacy ? filteredMedicines : visibleMedicines}
         />
