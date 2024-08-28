b439c481e7d04d121b236be1c308d2456b6e1d41
Marat Baikin <3371335@gmail.com>
2024-08-28 17:38:10 +0300

    Update Navbar.jsx

--- src/modules/Navbar/Navbar.jsx
+++ src/modules/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import MedicineSearchForm from 'modules/MedicineSearchForm/MedicineSearchForm';
@@ -8,28 +8,6 @@ import css from './Navbar.module.css';
 
 const Navbar = () => {
   const { filter, changeFilter } = useContext(CartContext);
-
-  // const changeFilter = event =>
-  //   (filter = event.currentTarget.value.toLowerCase());
-
-  // const [filter, setFilter] = useState('');
-
-  // const changeFilter = event => setFilter(event.currentTarget.value);
-
-  // const getVisibleMedicines = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return medicineItems.filter(({ name }) => {
-  //     return name.toLowerCase().includes(normalizedFilter);
-  //   });
-  // };
-
-  // const visibleMedicines = getVisibleMedicines();
-  // const handleFormSubmit = filter => {
-  //   setSearchParams({ searchQuery });
-  //   setMovieItems([]);
-  //   setPage(1);
-  // };
 
   return (
     <div className={css.navbar}>
@@ -46,8 +24,7 @@ const Navbar = () => {
             </NavLink>
           </li>
         </ul>
-        {/* <MedicineSearchForm value={filter} /> */}
-        {/* <MedicineSearchForm value={filter} onChange={changeFilter} /> */}
+
         <MedicineSearchForm value={filter} onChange={changeFilter} />
       </div>
     </div>
