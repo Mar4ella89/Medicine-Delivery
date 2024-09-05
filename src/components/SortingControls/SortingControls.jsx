e711f070d1a2d24eff23a7b9b40665c4aac1f244
Marat Baikin <3371335@gmail.com>
2024-09-05 17:11:14 +0300

    added style for sorting panel

--- src/components/SortingControls/SortingControls.jsx
+++ src/components/SortingControls/SortingControls.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import css from './SortingControls.module.css';
 
 const SortingControls = ({
@@ -6,19 +8,46 @@ const SortingControls = ({
   onSortFromExpensive,
   onSortByName,
 }) => {
+  const [activeSort, setActiveSort] = useState(null);
+
+  const handleClick = (sortType, sortFunction) => {
+    setActiveSort(sortType);
+    sortFunction();
+  };
+
   return (
     <div className={css.wrapper}>
       <span className={css.title}>Sorting:</span>
-      <span onClick={onFilterByFavorite} className={css.sortingItem}>
+      <span
+        onClick={() => handleClick('favorite', onFilterByFavorite)}
+        className={`${css.sortingItem} ${
+          activeSort === 'favorite' ? css.selected : ''
+        }`}
+      >
         Favorites
       </span>
-      <span onClick={onSortFromCheap} className={css.sortingItem}>
+      <span
+        onClick={() => handleClick('cheap', onSortFromCheap)}
+        className={`${css.sortingItem} ${
+          activeSort === 'cheap' ? css.selected : ''
+        }`}
+      >
         From cheap to expensive
       </span>
-      <span onClick={onSortFromExpensive} className={css.sortingItem}>
+      <span
+        onClick={() => handleClick('expencive', onSortFromExpensive)}
+        className={`${css.sortingItem} ${
+          activeSort === 'expencive' ? css.selected : ''
+        }`}
+      >
         From expensive to cheap
       </span>
-      <span onClick={onSortByName} className={css.sortingItem}>
+      <span
+        onClick={() => handleClick('name', onSortByName)}
+        className={`${css.sortingItem} ${
+          activeSort === 'name' ? css.selected : ''
+        }`}
+      >
         Sort by Name (A to Z)
       </span>
     </div>
